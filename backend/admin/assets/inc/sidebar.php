2bc18e4dc07099501e1834b87eaab31eab439904
Sunhadi <ahmadsunhadikamil65@gmail.com>
2024-08-28 17:39:19 +0700

    first commit

---
+++ backend/admin/assets/inc/sidebar.php
@@ -0,0 +1,318 @@
+<div class="left-side-menu">
+
+                <div class="slimscroll-menu">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+
+                        <ul class="metismenu" id="side-menu">
+
+                            <li class="menu-title">Navigation</li>
+
+                            <li>
+                                <a href="his_admin_dashboard.php">
+                                    <i class="fe-airplay"></i>
+                                    <span> Dashboard </span>
+                                </a>
+                                
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fab fa-accessible-icon "></i>
+                                    <span> Pasien </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_register_patient.php">Register Pasien</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_patients.php">tampilkan Pasien</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_patient.php">Mengelola Pasien</a>
+                                    </li>
+                                    <hr>
+                                    <li>
+                                        <a href="his_admin_discharge_patient.php">Mengeluarkan Pasien</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_patient_transfer.php">Transfer Pasien</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-doctor"></i>
+                                    <span> Pegawai </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_employee.php">tambah Pegawai</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_employee.php">Tampilkan Pegawai</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_employee.php">Mengelola Pegawai</a>
+                                    </li>
+                                    <hr>
+                                    <li>
+                                        <a href="his_admin_assaign_dept.php">Mengalokasikan Departemen</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_transfer_employee.php">Transfer Pegawai</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-pill"></i>
+                                    <span> Farmasi </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_pharm_cat.php">Tambah Kategori Farmasi</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_pharm_cat.php">Lihat Kategori Farmasi</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_pharm_cat.php">Mengelola Kategori Farmasi</a>
+                                    </li>
+                                    <hr>
+                                    <li>
+                                        <a href="his_admin_add_pharmaceuticals.php">Tambahkan Obat-obatan</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_pharmaceuticals.php">Tampilkan Obat-obatan</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_pharmaceuticals.php">Mengurus Obat-obatan</a>
+                                    </li>
+                                    <hr>
+                                    <li>
+                                        <a href="his_admin_add_presc.php">Menambahkan Resep Dokter</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_presc.php">Melihat Daftar Resep Dokter</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_presc.php">Mengelola Resep</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-cash-multiple "></i>
+                                    <span> akuntansi </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_acc.payable.php">Tambah Utang Pembayaran</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_acc_payable.php">Mengelola Utang Usaha</a>
+                                    </li>
+                                    <hr>
+                                    <li>
+                                        <a href="his_admin_add_acc_receivable.php">Tambah Piutang Usaha</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_acc_receivable.php">Mengelola Piutang Usaha</a>
+                                    </li>
+                                    <hr>
+                                    
+                                    
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class=" fas fa-funnel-dollar "></i>
+                                    <span> persediaan </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                
+                                    <li>
+                                        <a href="his_admin_pharm_inventory.php">produk farmasi</a>
+                                    </li>
+
+                                    <li>
+                                        <a href="his_admin_equipments_inventory.php">Aset</a>
+                                    </li>
+                                    
+                                </ul>
+                            </li>
+                
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fe-share"></i>
+                                    <span> Laporan </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_inpatient_records.php">InPatient Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_outpatient_records.php">OutPatient Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_employee_records.php">Employee Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_pharmaceutical_records.php">Pharmaceutical Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_accounting_records.php">Accounting Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_medical_records.php">Medical Records</a>
+                                    </li>
+                                    
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fe-file-text"></i>
+                                    <span> Medical Records </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_medical_record.php">Add Medical Record</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_medical_record.php">Manage Medical Records</a>
+                                    </li>
+                                    
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-flask"></i>
+                                    <span> Laboratory </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_patient_lab_test.php">Patient Lab Tests</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_patient_lab_result.php">Patient Lab Results</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_patient_lab_vitals.php">Patient Vitals</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_employee_lab_vitals.php">Employee Vitals</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_lab_report.php">Lab Reports</a>
+                                    </li>
+                                    <hr>
+                                    <li>
+                                        <a href="his_admin_add_lab_equipment.php">Add Lab Equipment</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_lab_equipment.php">Manage Lab Equipments</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-scissors-cutting "></i>
+                                    <span> Surgical / Theatre </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_equipment.php">Add Equipment</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_equipment.php">Manage Equipments</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_add_theatre_patient.php">Add Patient</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_theatre_patient.php">Manage Patients</a>
+                                    </li>
+
+                                    <li>
+                                        <a href="his_admin_surgery_records.php">Surgery Records</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-cash-refund "></i>
+                                    <span> Payrolls </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_payroll.php">Add Payroll</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_payrolls.php">Manage Payrolls</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_generate_payrolls.php">Generate Payrolls</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fas fa-user-tag"></i>
+                                    <span> Vendors </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_vendor.php">Add Vendor</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_vendor.php">Manage Vendors</a>
+                                    </li>
+                                    
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fas fa-lock"></i>
+                                    <span> Password Resets </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_manage_password_resets.php">Manage</a>
+                                    </li>
+                                                                        
+                                </ul>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                    <!-- End Sidebar -->
+
+                    <div class="clearfix"></div>
+
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sistem Informasi Rs Budi Sehat</title>
+    <link rel="shortcut icon" href="assets/images/logo/budi.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/animate-3.7.0.css">
+    <link rel="stylesheet" href="assets/css/font-awesome-4.7.0.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-4.1.3.min.css">
+    <link rel="stylesheet" href="assets/css/owl-carousel.min.css">
+    <link rel="stylesheet" href="assets/css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="assets/css/linearicons.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="preloader">
+        <div class="spinner"></div>
+    </div>
+    <header class="header-area">
+        <div id="header" id="home">
+            <div class="container">
+                <div class="row align-items-center justify-content-between d-flex">
+                    <div id="logo">
+                        <a href="index.php"></a>
+                    </div>
+                    <nav id="nav-menu-container">
+                        <ul class="nav-menu">
+                            <li class="menu-active"><a href="index.php">Beranda</a></li>
+                            <li><a href="backend/doc/index.php">Dokter Login</a></li>
+                            <li><a href="backend/admin/index.php">Admin Login</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="banner-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <h4>Sehat itu gak mahal</h4>
+                    <h1>Management Rumah Sakit Budi Sehat</h1>
+                    <p>Tetap Terdepan dalam Pelayanan Kesehatan, Perawatan dengan Hati, Pelayanan dengan Kualitas</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
+    <script src="assets/js/vendor/bootstrap-4.1.3.min.js"></script>
+    <script src="assets/js/vendor/wow.min.js"></script>
+    <script src="assets/js/vendor/owl-carousel.min.js"></script>
+    <script src="assets/js/vendor/jquery.datetimepicker.full.min.js"></script>
+    <script src="assets/js/vendor/jquery.nice-select.min.js"></script>
+    <script src="assets/js/vendor/superfish.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
